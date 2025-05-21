553798e02f25eea14dcf5fc75bd4e80aba86f833
Emmanuel <nwodoemmanuel@gmail.com>
2025-05-21 11:13:15 +0100

    fetched data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -2,7 +2,16 @@ import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 import CheckMarkIcon from "../assets/images/icons/checkmark.png";
+
 export function HomePage() {
+  fetch("http://localhost:3000/api/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="images/home-favicon.png" />
